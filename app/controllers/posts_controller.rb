324e1fecdb500734b79a2205121039cbce3ddb2a
reggordon <reg@reggordon.com>
2015-11-10 20:34:25 +0000

    this better work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
-class PostsController < ApplicationController  
+
+class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :owned_post, only: [:edit, :update, :destroy]  
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :like]
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -9,14 +10,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    
   end
 
   def new
-   
-      @post = current_user.posts.build
-
+     @post = current_user.posts.build
   end
+
 
   def create
     @post = current_user.posts.build(post_params)
@@ -30,41 +29,41 @@ class PostsController < ApplicationController
     end
   end
 
+
   def edit
   end
 
   def update
-    @post.update(post_params)
-      redirect_to posts_path
-    
-  end
-
-  def destroy
-    if @post.destroy
-        flash[:success]= "you have killed it"
-    redirect_to posts_path
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to root_path
     else
-    flash.now[:alert] ="you havent killed it yet"
-    redirect_to posts_path
+      flash[:alert] = "Update failed.  Please check the form."
+      render :edit
     end
   end
 
-  private
+  def destroy
+    @post.destroy
+    flash[:success] = "Your post has been deleted."
+    redirect_to root_path
+  end
+
+private
 
   def post_params
     params.require(:post).permit(:image, :caption)
   end
 
-
-
   def set_post
     @post = Post.find(params[:id])
   end
-end
 
-def owned_post  
-  unless current_user == @post.user
-    flash[:alert] = "That post doesn't belong to you!"
-    redirect_to root_path
+  def owned_post
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
   end
-end  
+
+end
